3be9bfe4caf070e7bf2b764b35c6f2bed146f8f6
jvgar10 <maddogjig@hotmail.com>
2023-09-16 00:29:03 -0500

    finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,7 +8,7 @@ using System;
         string first = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last = Console.ReadLine()
+        string last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
