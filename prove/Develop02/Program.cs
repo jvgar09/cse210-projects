7411c14666d3735e7cf5023e7da5776ebf1e8df1
jvgar10 <maddogjig@hotmail.com>
2023-10-05 21:32:12 -0500

    add

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,83 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-
-class Entry
-{
-    public string Prompt { get; set; }
-    public string Response { get; set; }
-    public string Date { get; set; }
-}
-
-class Journal
-{
-    private List<Entry> entries;
-
-    public Journal()
-    {
-        entries = new List<Entry>();
-    }
-
-    public void AddEntry(string prompt, string response, string date)
-    {
-        entries.Add(new Entry { Prompt = prompt, Response = response, Date = date });
-    }
-
-    public void DisplayEntries()
-    {
-        foreach (var entry in entries)
-        {
-            Console.WriteLine($"Date: {entry.Date}");
-            Console.WriteLine($"Prompt: {entry.Prompt}");
-            Console.WriteLine($"Response: {entry.Response}");
-            Console.WriteLine();
-        }
-    }
-
-    public void SaveToFile(string fileName)
-    {
-        using (StreamWriter writer = new StreamWriter(fileName))
-        {
-            foreach (var entry in entries)
-            {
-                writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
-            }
-        }
-        Console.WriteLine("Journal saved to file.");
-    }
-
-    public void LoadFromFile(string fileName)
-    {
-        if (File.Exists(fileName))
-        {
-            entries.Clear(); // Clear existing entries before loading
-            using (StreamReader reader = new StreamReader(fileName))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    string[] parts = line.Split('|');
-                    if (parts.Length == 3)
-                    {
-                        entries.Add(new Entry { Date = parts[0], Prompt = parts[1], Response = parts[2] });
-                    }
-                }
-            }
-            Console.WriteLine("Journal loaded from file.");
-        }
-        else
-        {
-            Console.WriteLine("File not found.");
-        }
-    }
-}
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main()
     {
         Journal journal = new Journal();
+        
 
         while (true)
         {
@@ -88,81 +16,42 @@ class Program
             Console.WriteLine("4. Load ");
             Console.WriteLine("5. Exit ");
             Console.Write("Enter your choice: ");
+            option = int.Parse(Console.ReadLine());
+            if (option < 1 || option > 5)
+            {
+                Console.WriteLine{"Please choose a number between 1-5."};
+            }
+            else if (option == 1)
+            {
+                Entry entry = new Entry();
+                PromptGenerator promptGenerator = new PromptGenerator();
+                DateTime currentDate = DateTime.Now;
 
-            int choice;
-            if (int.TryParse(Console.ReadLine(), out choice))
-            {
-                switch (choice)
-                {
-                    case 1:
-                        Console.Write("Enter response: ");
-                        string response = Console.ReadLine();
-                        Console.Write("Enter date: ");
-                        string date = Console.ReadLine();
-                        // Generate a random prompt from your list
-                        string[] prompts = { "Prompt 1", "Prompt 2", "Prompt 3", "Prompt 4", "Prompt 5" };
-                        Random rand = new Random();
-                        string randomPrompt = prompts[rand.Next(prompts.Length)];
-                        journal.AddEntry(randomPrompt, response, date);
-                        break;
-                    case 2:
-                        journal.DisplayEntries();
-                        break;
-                    case 3:
-                        Console.Write("Enter file name to save: ");
-                        string saveFileName = Console.ReadLine();
-                        journal.SaveToFile(saveFileName);
-                        break;
-                    case 4:
-                        Console.Write("Enter file name to load: ");
-                        string loadFileName = Console.ReadLine();
-                        journal.LoadFromFile(loadFileName);
-                        break;
-                    case 5:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice. Please try again.");
-                        break;
-                }
+                entry,_date = currentDate.ToShortDateString();
+                entry._prompt = promptGenerator.Display();
+                Console.WriteLine(entry._date);
+                Console.WriteLine(entry._prompt);
+                entry._answer = Console.ReadLine();
+                journal._entries.Add(entry);
             }
-            else
+            else if (option == 2)
             {
-                Console.WriteLine("Invalid input. Please enter a number.");
+                journal.Display();
             }
+            else if (option == 3)
+            {
+                Console.WriteLine("What is the name of the file that you will like to save?")
+                journal._fileName = Console.ReadLine();
+                journal.SaveFile();
+            
+            }
+            else if (option == 4) 
+            {
+                Console.WriteLine("What is the name of the file that you will like to load?")
+                journal._fileName = Console.ReadLine();
+                journal.LoadFile();
+            }
+
         }
     }
 }
-
-// Create a class that represents the structure of your CSV data
-public class MyCsvData
-{
-    public string Column1 { get; set; }
-    public string Column2 { get; set; }
-   
-}
-
-
-// Read CSV data
-var csvRecords = new List<MyCsvData>();
-using (var reader = new StreamReader("promts.csv"))
-using (var csv = new CsvReader(reader, new CsvConfiguration (CultureInfo.InvariantCulture)))
-{
-    csvRecords = csv.GetRecords<MyCsvData>().ToList();
-}
-
-
-// Read text from a .txt file
-string filePath = "prompts.txt";
-if (File.Exists(filePath))
-{
-    string text = File.ReadAllText(filePath);
-    Console.WriteLine("Text from the file:");
-    Console.WriteLine(text);
-}
-else
-{
-    Console.WriteLine("File not found.");
-}
-
-Console.ReadKey();
---
+++ prove/Develop02/PrompGenerator.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class PromptGenerator
+{
+    private Random _random = new Random();
+    private string [] _prompts; 
+    public PromptGenerator()
+    { 
+        _prompts = new string[]
+        {
+            "Who was the most interesting person I interacted with today?",     
+            "What was the best part of my day?",
+            "How did I see the hand of the Lord in my life today?",
+            "What was the strongest emotion I felt today?",
+            "If I had one thing I could do over today, what would it be?",
+            "Who was I nice to today, what did I do for them?",
+            "What did I learn form my scripture reading?",
+            "What brings you joy?",
+            "What is something that you would like to change about yourself? How can you change it?",
+            "What happened to me today?",
+            "What was the most challenging thing I faced today?",
+            "What did I learn today?",
+            "What was the most fun thing I did today?",
+            "What was the most surprising thing that happened today?",
+        }; 
+    }
+    public string Display()
+    {
+        int index = _random.Next{0, _prompts.Length};
+        return _prompts[index];
+    }
+
+}
