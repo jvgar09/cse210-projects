02543f9590cf6089673c0e37289d1f3fb6e619cb
jvgar10 <maddogjig@hotmail.com>
2023-10-20 23:08:52 -0500

    add

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -12,8 +12,8 @@ public class Program
         Dictionary<int, Dictionary<string, string>> scriptures = new()
         {
             {1, new Dictionary<string, string> {
-                {"Text", "And it came to pass that I, Nephi, said unto my father: I will go and do the things which  the Lord hath commanded, for I know that the Lord giveth no commandments unto  the children of men,  save he shall prepare a way for them that they may accomplish the thing which he commandeth them."},
-                {"Reference", "1 Nephi 3:7"}}},
+                {"Text", "Therefore go, my son, and thou shalt be favored of the Lord, because thou hast anot bmurmured. And it came to pass that I, Nephi, said unto my father: I will go and do the things which  the Lord hath commanded, for I know that the Lord giveth no commandments unto  the children of men,  save he shall prepare a way for them that they may accomplish the thing which he commandeth them."},
+                {"Reference", "1 Nephi 3:6-7"}}},
             {2, new Dictionary<string, string> {
                 {"Text", "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths."},
                 {"Reference", "Proverbs 3:5-6"}}}
