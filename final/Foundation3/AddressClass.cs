ad6ad8c06f71e72e8c503a84cca7a428a7da1b03
jvgar09 <144757138+jvgar09@users.noreply.github.com>
2023-11-24 19:04:39 -0600

    Update AddressClass.cs

--- final/Foundation3/AddressClass.cs
+++ final/Foundation3/AddressClass.cs
@@ -1,16 +1,1 @@
-// Address class
-class Address
-{
-    public string _Street { get; set; }
-    public string _City { get; set; }
-    public string _State { get; set; }
-    public string _ZipCode { get; set; }
 
-    public Address(string street, string city, string state, string zipCode)
-    {
-        _Street = street;
-        _City = city;
-        _State = state;
-        _ZipCode = zipCode;
-    }
-}
