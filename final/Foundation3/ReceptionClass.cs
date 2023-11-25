1b01ca4b58f23febf3c4df2e0d4b8e667a00974e
jvgar09 <144757138+jvgar09@users.noreply.github.com>
2023-11-24 19:05:57 -0600

    Update ReceptionClass.cs

--- final/Foundation3/ReceptionClass.cs
+++ final/Foundation3/ReceptionClass.cs
@@ -1,16 +1,1 @@
-// Reception class
-class Reception : Event 
-{
-    private string _rsvpEmail;
 
-    public Reception(string title, string description, DateTime date, string time, Address address, string rsvpEmail)
-        : base(title, description, date, time, address)
-    {
-        _rsvpEmail = rsvpEmail;
-    }
-
-    public new string GetFullDetails()
-    {
-        return $"{base.GetFullDetails()}\nType: Reception\nRSVP Email: {_rsvpEmail}";
-    }
-}
