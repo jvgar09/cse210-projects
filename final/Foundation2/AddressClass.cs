d53e7ce3a397f7bf63e9bec6bbd09927add7c34a
jvgar09 <144757138+jvgar09@users.noreply.github.com>
2023-11-24 19:18:09 -0600

    fix AddressClass.cs

--- final/Foundation2/AddressClass.cs
+++ final/Foundation2/AddressClass.cs
@@ -1,29 +1,29 @@
 class Address // class object 
 {
     // access modifier, data type, variable;
-    private string street;
-    private string city;
-    private string state;
-    private string country;
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _country;
 
     // Construoctor
     public Address(string street, string city, string state, string country)
     {
-        this.street = street;
-        this.city = city;
-        this.state = state;
-        this.country = country;
+        _street = street;
+        _city = city;
+        _state = state;
+        _country = country;
     }
 
     public bool IsInUSA() 
     {
         // Returns true if the country is "USA", otherwise false
-        return country.Equals("USA", StringComparison.OrdinalIgnoreCase);
+        return _country.Equals("USA", StringComparison.OrdinalIgnoreCase);
     }
 
     public override string ToString()
     {
-        return $"{street}, {city}, {state}, {country}";
+        return $"{_street}, {_city}, {_state}, {_country}";
     }
 }
 
