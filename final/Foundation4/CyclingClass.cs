8999493228924780f8593a16d59fe7aa3171dbe2
jvgar09 <144757138+jvgar09@users.noreply.github.com>
2023-11-24 19:07:34 -0600

    Update CyclingClass.cs

--- final/Foundation4/CyclingClass.cs
+++ final/Foundation4/CyclingClass.cs
@@ -1,19 +1,1 @@
-class Cycling : Activity  //inheritance 
-{
-    private double _speed; // attribute 
 
-    public Cycling(DateTime date, int minutes, double speed) : base(date, minutes)
-    {
-        _speed = speed;
-    }
-
-    public override double GetSpeed() //behavors/methods 
-    {
-        return _speed;
-    }
-
-    public override double GetPace()
-    {
-        return 60 / _speed;
-    }
-}
