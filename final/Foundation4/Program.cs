d834ea20e5944d61f83e692b712b9589e82d49bd
jvgar09 <144757138+jvgar09@users.noreply.github.com>
2023-11-24 19:07:49 -0600

    Update Program.cs

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,18 +1,1 @@
-using System;
-using System.Collections.Generic;
-class Program
-{
-    static void Main()
-    {
-        List<Activity> activities = new List<Activity>();
 
-        activities.Add(new Running(new DateTime(2022, 11, 03), 30, 3.0));
-        activities.Add(new Cycling(new DateTime(2022, 11, 04), 45, 15.0));
-        activities.Add(new Swimming(new DateTime(2022, 11, 05), 60, 20));
-
-        foreach (Activity activity in activities)
-        {
-            Console.WriteLine(activity.GetSummary());
-        }
-    }
-}
