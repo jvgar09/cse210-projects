6a390cea7438d9888fa4cf0ddf7e219c8cbaa5e9
jvgar09 <144757138+jvgar09@users.noreply.github.com>
2023-11-24 19:20:45 -0600

    fix ProductClass.cs

--- final/Foundation2/ProductClass.cs
+++ final/Foundation2/ProductClass.cs
@@ -1,25 +1,25 @@
 class Product // class object 
 {
     // access modifier, data type, variable;
-    private string name;
-    private int productId;
-    private double price; //floating-point data type (used for storing values such as prices, percentages, or any numeric value requiring decimal precision.)
-    private int quantity;
+    private string _name;
+    private int _productId;
+    private double _price; //floating-point data type (used for storing values such as prices, percentages, or any numeric value requiring decimal precision)
+    private int _quantity;
 
     // Construoctor
     public Product(string name, int productId, double price, int quantity)
     {
-        this.name = name;
-        this.productId = productId;
-        this.price = price;
-        this.quantity = quantity;
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
     }
 
     public double CalculatePrice() //double is a data type that represents a floating-point number
     {
-        return price * quantity;
+        return _price * _quantity;
     }
 
-    public string Name { get { return name; } }
-    public int ProductId { get { return productId; } }
+    public string Name { get { return _name; } }
+    public int ProductId { get { return _productId; } }
 }
